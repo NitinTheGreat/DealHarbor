8e6766e3be0ca3170e3045eef85f437317222974
Nitinthegreat <nitinpandey1304@gmail.com>
2024-06-23 20:10:27 +0530

    spline done for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import "../styles/page.css";
 export default function Home() {
   return (
     <>
-      <iframe className="spline" src='https://my.spline.design/purple3diconscopy-b50e3c8fb74cd3d019ce5f11de2f0bbc/' frameBorder='0' width='100%' height='100%'></iframe>
+    <iframe className="spline" src='https://my.spline.design/purple3diconscopy-b50e3c8fb74cd3d019ce5f11de2f0bbc/' frameBorder='0' width='100%' height='100%'></iframe>
+      {/* <iframe className="spline" src='https://my.spline.design/purple3diconscopy-b50e3c8fb74cd3d019ce5f11de2f0bbc/' frameBorder='0' width='100%' height='100%'></iframe> */}
     </>
   );
 }
