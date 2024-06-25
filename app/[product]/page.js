5fd90fd773f4651060f3651856e3327ec497fa8c
Nitinthegreat <nitinpandey1304@gmail.com>
2024-06-25 10:58:47 +0530

    .

--- app/[product]/page.js
+++ app/[product]/page.js
@@ -10,7 +10,7 @@ const product = ({params}) => {
     <div className="lg:w-4/5 mx-auto flex flex-wrap">
       <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400" />
       <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-        <h2 className="text-sm title-font text-gray-500 tracking-widest">By Seller's Name</h2>
+        <h2 className="text-sm title-font text-gray-500 tracking-widest">By Seller Name</h2>
         <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{params.product}</h1>
         <div className="flex mb-4">
           <span className="flex items-center">
